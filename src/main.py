e3b558e957b135b9842e357265603b3910272bd7
backtr4ck <rgh0993@gmail.com>
2023-08-01 19:02:09 -0300

    feat: fixed CORS

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 
 from routers import auth, reservations
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # routers
 app.include_router(reservations.router)
